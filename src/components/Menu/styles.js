7e8a75ac11f713ee233173fdd662b5d4bbc10ce1
Mateus <mateus1348@hotmail.com>
2022-04-12 01:19:56 -0300

    Alterações no style do Menu

--- src/components/Menu/styles.js
+++ src/components/Menu/styles.js
@@ -1,14 +1,11 @@
 import { StyleSheet } from 'react-native';
 
 export const styles = StyleSheet.create({
-    container:{
-        flex: 1,
+    container:{      
+        width: '100%',
         borderWidth: 1,
-        backgroundColor: 'white',
-        height: 60,
-        paddingHorizontal: 20,
         flexDirection: 'row',
         alignItems: 'center',
-        justifyContent: 'space-between',
+        justifyContent: 'space-between'
     }
 });
