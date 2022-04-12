6c971e11223390262da5ea07152ad0655f6d1956
Mateus <mateus1348@hotmail.com>
2022-04-12 02:44:33 -0300

    Organizando os styles de VerificarNumeros

--- src/components/VerificarNumeros/index.js
+++ src/components/VerificarNumeros/index.js
@@ -1,48 +1,40 @@
 import React from 'react';
 import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
 
+import { styles } from './styles';
+
 const VerificarNumeros = () => {
   
     return (
         <View style={styles.container}>
-            <Text style={styles.text}>Verificando os números certos</Text>
-            <Text>Digite os números abaixo</Text>
+
+            <Text style={styles.tittle}>Será que digitos </Text>
+            <Text style={styles.tittle}>estão na casa certa?</Text>
+
             <TextInput style={styles.input}
-                placeholder='De 0 à 10'
+                maxLength={1}
+                placeholder='De 0 à 9'
                 keyboardType='numeric'
             />
+
             <TextInput style={styles.input}
-                placeholder='De 11 à 19'
+                maxLength={2}
+                placeholder='De 10 à 19'
                 keyboardType='numeric'
             />
+
             <TextInput style={styles.input}
-                placeholder='Mais 20'
+                maxLength={2}
+                placeholder='De 20 à 50'
                 keyboardType='numeric'
             />
+
             <Button
                 title="Verificar"
             />
+
         </View>
     );
 };
-
-const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
-      padding: 50,
-    },
-    input: {
-        height: 20,
-        margin: 5,
-        borderWidth: 1,
-        padding: 10,
-    },
-    text:{
-        fontSize: 20,
-    },
-});
 
 export default VerificarNumeros;
---
+++ src/components/VerificarNumeros/styles.js
@@ -0,0 +1,20 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+        padding: 50
+    },
+    tittle:{
+        fontSize: 20
+    },
+    input: {
+        width: 80,
+        height: 25,
+        margin: 5,
+        borderWidth: 1
+    }
+});
